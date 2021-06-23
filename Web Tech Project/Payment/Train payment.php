6c8c998b56017a009350188c9e548a47ae2596b9
MD. Mohiuddin ahmed efti <ahmedefti277@gmail.com>
2021-06-24 00:27:06 +0600

    Project update

--- Web Tech Project/Payment/Train payment.php
+++ Web Tech Project/Payment/Train payment.php
@@ -2,8 +2,8 @@
 
 
 <html>
-	<body><title>Train Search</title>
-	<fieldset><legend align="center"><h1>Train Search</h1></legend>
+	<body><title>Train</title>
+	<fieldset><legend align="center"><h1>Train</h1></legend>
 	<a href="..\HomePage.php">Go Back To Home</a>
 	<form action="" method="post">
 	<form border>
@@ -11,31 +11,10 @@
 	<table align = "center">
 	<tr>
 		<td>
-			<p align="center">Departure</p>
-			<input type="submit" value="< Prev. Day">
-			<input type="submit" value="Next Day>">
-		</td>
-		<td>
-		</td>
-		<td>
-			<p align="center">Return (Optional)</p>
-			<input type="submit" value="< Prev. Day">
-			<input type="submit" value="Next Day>">
-		</td>
-	</tr>
-	<tr>
-		<td>
 		Train Type
 		</td>
 		<td>
-		<select>
-		<option disabled selected>Train Type</option>
-			
-			<option>1st Class</option>
-			<option>2nd Class</option>
-			<option>3rd Class</option>
-		</select>
-
+		3rd Class
 		</td>
 	</tr>
 	<tr>
@@ -43,14 +22,7 @@
 		Departure Time
 		</td>
 		<td>
-		<select>
-		    <option disabled selected>Departure Time</option>
-			<option>Before 06:00 AM</option>
-			<option>06:00 AM - 12:00 PM</option>
-			<option>12:00 PM - 06:00 PM</option>
-			<option>After 06:00 PM</option>
-		</select>
-		
+		29June , 06:45 AM		
 		</td>
 	</tr>
 	<tr>
@@ -58,52 +30,43 @@
 		Arrival Time
 		</td>
 		<td>
-		<select>
-			<option disabled selected>Arrival Time</option>
-			<option>Before 06:00 AM</option>
-			<option>06:00 AM - 12:00 PM</option>
-			<option>12:00 PM - 06:00 PM</option>
-			<option>After 06:00 PM</option>
-		</select>
+		05July , 12:15 PM
+		</td>
+	</tr>
+	<tr>
+		<td>
+		Seat Numbers Are:
+		</td>
+		<td>
+		
+			101<br>
+			102<br>
+			103<br>
+			104<br>
+			
+		
 		
 		</td>
 	</tr>
 	<tr>
 		<td>
-		No. Of Seat
-		</td>
-		<td>
-		<select>
-			<option disabled selected>No. Of Seat</option>
-			<?php
-										for($i1=1;$i1<=10;$i1++)
-										{
-											echo "<option>$i1</option>";
-										}
-			?>
-		</select>
-		
-		</td>
-	</tr>
-	<tr>
-		<td>
-		Your Desire Train
-		</td>
-		<td>
-		
-					<input type="checkbox" value="Ekota Express">Ekota Express<br>
-					<input type="checkbox" value="Tista Express">Tista Express<br>
-							
+		Train Found<br>
+		Select One Train
 		</td>
 		<td>
 					<input type="checkbox" value="Parabat Express">Parabat Express<br>
 					<input type="checkbox" value="Upakul Express">Upakul Express<br>
-					
+		</td>
+	</tr>
+	
+	<tr>
+		<td colspan="2" align="center">
+			<h1>Tk 1500</h1>
 		</td>
 	</tr>
 	<tr>
-		<td colspan="3" align="center">
-			<input type="submit" value="Search">
+		<td colspan="2" align="center">
+			<input type="submit" value="Confirm">
 		</td>
 	</tr>
 	</table>
